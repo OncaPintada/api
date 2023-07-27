0a569c1849b582f55af03b1d883a0f4bcf1c2154
Zanelato <pedeozanelato19@gmail.com>
2023-07-26 21:25:44 -0400

    API aprimorada para receber atributo amount

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ from pydantic import BaseModel
 from joblib import load
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-model_RandomForest = load('./models/randomForest_Oversampling_FeatureSelection.joblib') 
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+model_RandomForest = load('./models/randomForest_Oversampling_FeatureSelection2.joblib') 
 class Item(BaseModel):
     index: list
     v7: list
@@ -18,15 +34,16 @@ class Item(BaseModel):
     v4: list
     v10: list
     v14: list
+    amount: list
 
 class Items(BaseModel):
     listItems: object
 
 @app.put("/classificationSingle")
-def update_item(item: Item):
+def single(item: Item):
     df = item.model_dump()
     df = pd.DataFrame.from_dict(df)
-    df = df.iloc[:,1:]
+    df = df.iloc[:,1:-1]
     classification = model_RandomForest.predict(df)
     print(type(classification))
     classification = classification.tolist()
@@ -34,11 +51,11 @@ def update_item(item: Item):
 
 
 @app.put("/classificationAll")
-def update_item(items: Items):
+def all(items: Items):
     df = items.model_dump()
     df = pd.DataFrame(df['listItems'])
     df = df.transpose()
-    df = df.iloc[:,1:]
+    df = df.iloc[:,1:-1]
     classification = model_RandomForest.predict(df)
     classification = classification.tolist()
     return {"classification":classification}
